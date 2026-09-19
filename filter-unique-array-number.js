cc09e263d2dc14cb848882a62bdff9bf0fb326d1
Kuma-369 <191901457+Kuma-369@users.noreply.github.com>
2026-09-19 19:37:50 +0100

    Complete filter unique array number

---
+++ filter-unique-array-number.js
@@ -0,0 +1,14 @@
+// Let arr be an array.
+// Create a function unique(arr) that should return an array with unique items of arr.
+
+function unique(arr) {
+  /* your code */
+  let uniqueSet = new Set(arr);
+  return uniqueSet;
+}
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+console.log( unique(strings) ); // Hare, Krishna, :-O
